4c952536f1a462e7448d044c1ab6b30db5e51146
yurlovm <yurlovm@gmail.com>
2017-04-01 21:21:59 +0500

    Refactor to be more reactive

--- WebApiAngular/src/app/core/services/signalr.service.ts
+++ WebApiAngular/src/app/core/services/signalr.service.ts
@@ -1,33 +1,59 @@
-import { Injectable } from '@angular/core';
-import { AuthService } from './auth.service';
-import { SignalR, IConnectionOptions, ISignalRConnection, ConnectionStatus }  from 'ng2-signalr';
+import { Injectable }                                       from '@angular/core';
+import { AuthService }                                      from './auth.service';
+import { Observable }                                       from 'rxjs/Observable';
+import { Subject }                                          from 'rxjs/Subject';
+import { SignalR, IConnectionOptions, ISignalRConnection }  from 'ng2-signalr';
 
 @Injectable()
 export class SignalRService {
   private signalrConnection: ISignalRConnection;
   private connectionStatus = 'disconnected';
-  constructor(private authService: AuthService, private _signalR: SignalR) { }
+  private mainObs = new Subject<boolean>();
+  private stopFromCode = false;
+
+  constructor(private authService: AuthService, private _signalR: SignalR) {
+    const signalrObs = this.mainObs
+      .filter(x => x === true)
+      .filter(() => this.connectionStatus === 'disconnected')
+      .map((): IConnectionOptions => this.getConnectionOptions)
+      .switchMap((options: IConnectionOptions) =>
+        Observable.fromPromise(this._signalR.connect(options))
+          .retry(3)
+          .catch(err => Observable.empty())
+      )
+      .share();
+
+    signalrObs.subscribe((x: ISignalRConnection) => {
+      this.signalrConnection = x;
+      this.sendMessageToServer('test message');
+    }, (errmsg) => {
+      console.error(errmsg);
+    });
+
+    signalrObs.mergeMap((x: ISignalRConnection) => x.listenFor('echoMethodResponse'))
+      .subscribe((data: any) => {
+          console.log(data);
+        }, (errmsg: any) => {
+          console.error(errmsg);
+        });
+
+    signalrObs.mergeMap((x: ISignalRConnection) => x.status)
+      .map((x) => this.connectionStatus = x.name)
+      .filter((statusName) => statusName === 'disconnected')
+      .filter(() => !this.stopFromCode)
+      .delay(5000)
+      .subscribe((statusName: string) => {
+        this.connectToMessageHub();
+    });
+
+    signalrObs.mergeMap((x: ISignalRConnection) => x.errors)
+      .subscribe((error: any) => {
+        console.error('SignalR ERROR: ', error);
+      });
+  }
 
   public connectToMessageHub() {
-    if (this.connectionStatus !== 'disconnected') {
-      return;
-    }
-
-    const token = this.authService.getToken();
-    let options: IConnectionOptions = { hubName: 'messageHub', qs: {authorization: token} };
-    this._signalR.connect(options)
-      .then((signalrConnection) => {
-        this.signalrConnection = signalrConnection;
-        this.subToSatus();
-        this.subToErrors();
-        this.registerListeners();
-      })
-      .then(() => {
-        this.sendMessageToServer('test message');
-      })
-      .catch((errmsg) => {
-        console.error(errmsg);
-      });
+    this.mainObs.next(true);
   }
 
   public sendMessageToServer(msg: string) {
@@ -40,47 +66,27 @@ export class SignalRService {
     });
   }
 
-  private registerListeners() {
-    this.signalrConnection
-      .listenFor('echoMethodResponse')
-      .subscribe(
-        (data: any) => {
-          console.log(data);
-        },
-        (errmsg: any) => {
-          console.error(errmsg);
-        }
-      );
-  }
-
   public stopConnection() {
     if (this.signalrConnection && this.signalrConnection.id && this.connectionStatus !== 'disconnected') {
       this.signalrConnection.stop();
+      this.stopFromCode = true;
     }
   }
 
   public startConnection() {
     if (this.signalrConnection && this.signalrConnection.id && this.connectionStatus === 'disconnected') {
       this.signalrConnection.start();
+      this.stopFromCode = false;
     }
   }
 
-  private subToSatus() {
-    this.signalrConnection.status.subscribe((status: ConnectionStatus) => {
-      this.connectionStatus = status.name;
-      if (status.name === 'disconnected') {
-        setTimeout(() => {
-          this.connectToMessageHub();
-        }, 5000);
+  private getConnectionOptions() {
+    return {
+      hubName: 'messageHub',
+      qs: {
+        authorization: this.authService.getToken()
       }
-   //   console.log(status);
-    });
-  }
-
-  private subToErrors() {
-    this.signalrConnection.errors.subscribe((error: any) => {
-      console.error('SignalR ERROR: ', error);
-    });
+    };
   }
 
 }
--- WebApiAngular/src/vendor.ts
+++ WebApiAngular/src/vendor.ts
@@ -1,14 +1,11 @@
 import 'expose-loader?jQuery!../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/signalr/jquery.signalR.min.js';
 
-// Statics
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/retry';
-
-// Operators
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
@@ -19,3 +16,5 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/share';
 import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/empty';
+
